7ef46a55dc8cbe637768df1474e7923ac8c0eeca
balazshimself <balazshimself@gmail.com>
2025-02-12 20:02:52 +0100

    First Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
+import json
 import streamlit as st
+from google.cloud import firestore
 
-st.title("🎈 My new app")
+# Load Firestore credentials from Streamlit secrets
+firestore_credentials = json.loads(st.secrets["gcp_service_account"])
+db = firestore.Client.from_service_account_info(firestore_credentials)
+
+# Function to log predictions
+def log_prediction(inputs, prediction):
+    log_data = {
+        "features": inputs,
+        "prediction": prediction,
+        "timestamp": firestore.SERVER_TIMESTAMP
+    }
+    db.collection("logs").add(log_data)  # Store in Firestore
+
+st.title("My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
